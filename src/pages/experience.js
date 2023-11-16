9784a799607d78c51c119bd834517204b2dc6393
kayemba <kayembejonathan5@gmail.com>
2023-11-16 13:54:39 +0200

    Add the experience page

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 
 const Experience = () => {
     return(
-        <h1>contact</h1>
+        <div className="experience">
+          <VerticalTimeline lineColor="#3e497a">
+             <VerticalTimelineElement className="vertical-timeline-element--education"
+               date="2022 - 2023"
+               iconStyle={{background: "#3e497a", color: "#fff"}}
+             >
+              <h3 className="vertical-timeline-element-title">
+                My Random High School, Random Place, Random State
+              </h3>
+              <h4>College School Diploma</h4>
+            </VerticalTimelineElement>
+          </VerticalTimeline>
+        </div>
     )
 }
 
