9c9f660c9b6261f10354a5ad809b0aca05719dac
kayemba <kayembejonathan5@gmail.com>
2023-12-04 13:50:38 +0200

    Add fix the css

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -3,6 +3,8 @@ import '../pages/portfolio.css';
 import project1 from '../Assets/project1.png';
 import calulator from '../Assets/calulator.png';
 import Menufood from '../Assets/Menufood.png';
+import books from '../Assets/books.png';
+import pfolio from '../Assets/pfolio.png';
 
 const Portfolio = () => {
     return(
@@ -13,6 +15,8 @@ const Portfolio = () => {
               <a className="image" href="https://github.com/KAYEMBAJONATHAN/React-t"><img src={project1} alt="countrymap" /></a>
               <a className="image" href="https://github.com/mohamedSabry0/space_travelers_hub"><img src={calulator} alt="travelhub" /></a>
               <a className="image" href="https://github.com/Elhambasir/Food-Menu"><img src={Menufood} alt="foodmenu" /></a>
+              <a className="image" href="https://github.com/HenaryKhuraijam/bookList"><img src={books} alt="foodmenu" /></a>
+              <a className="image" href="https://kayembajonathan.github.io/Desk/"><img src={pfolio} alt="foodmenu" /></a>
             </div>
           </div>    
         </section>
